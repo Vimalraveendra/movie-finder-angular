a9e7128389a9ce68368f680d0b19642e4eeadd99
Vimal Raveendran <vimalraveendra@gmail.com>
2025-01-22 20:23:56 +0100

    added SearchMovies Container & its styles, updated markup of App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from "./header/header.component";
+import { SearchMoviesComponent } from "./search-movies/search-movies.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ HeaderComponent],
+  imports: [HeaderComponent, SearchMoviesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
